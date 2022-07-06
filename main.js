f42eb4009820627f104457a11015e9b21ac7bbdd
Robert Elinam Hlovor <100922061+rhlovor@users.noreply.github.com>
2022-07-06 10:39:52 +0000

    sss

---
+++ main.js
@@ -0,0 +1,14 @@
+const replyButton = document.querySelectorAll('.rep2');
+const commentBox = document.querySelector('#user-comment-box');
+
+/*for (let i = 0; i < replyButton.length; i++) {
+    replyButton[i].style.backgroundColor = "red";
+  }*/
+
+  let clone = commentBox.cloneNode(true);
+function userCommentBox(){
+    let newDiv = document.createElement('div');
+    
+
+}
+  replyButton.addEventListner('click', );
